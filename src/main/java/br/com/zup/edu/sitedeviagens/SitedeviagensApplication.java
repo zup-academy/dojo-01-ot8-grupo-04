5a68131edb232129b05af08982c7cf1c48a3e611
paula.santana <paula.santana@zup.com.br>
2021-08-23 21:20:37 -0300

    Setup do Projeto

---
+++ src/main/java/br/com/zup/edu/sitedeviagens/SitedeviagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.sitedeviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitedeviagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitedeviagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/sitedeviagens/SitedeviagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.sitedeviagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitedeviagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
