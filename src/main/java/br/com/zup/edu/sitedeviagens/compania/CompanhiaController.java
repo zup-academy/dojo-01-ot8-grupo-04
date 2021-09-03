27299aa483d590258019b1df5316f03a55f402bb
viniciusferreira <vinicius.ricardo@zup.com.br>
2021-09-03 11:15:44 -0300

    Ultimo commit DOJO

--- src/main/java/br/com/zup/edu/sitedeviagens/compania/CompanhiaController.java
+++ src/main/java/br/com/zup/edu/sitedeviagens/compania/CompanhiaController.java
@@ -1,7 +1,5 @@
 package br.com.zup.edu.sitedeviagens.compania;
 
-import br.com.zup.edu.sitedeviagens.pais.Pais;
-import br.com.zup.edu.sitedeviagens.pais.PaisForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +25,4 @@ public class CompanhiaController {
         URI uri = uriBuilder.path("/companhia/{id}").buildAndExpand(companhia.getId()).toUri();
         return ResponseEntity.created(uri).body(companhia.toString());
     }
-
-
 }
